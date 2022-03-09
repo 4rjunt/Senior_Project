c9b0e766ad0b2407b9cf6d4338223a96e445ce69
4rjunt <arielnunez040@gmail.com>
2022-03-08 19:06:46 -0500

    Update of the TimeTrack Class
    Now runs multiples functions at the same time.

--- Code/TimeTrackClass.py
+++ Code/TimeTrackClass.py
@@ -1,38 +1,54 @@
 from win32gui import GetForegroundWindow
+import win32gui
 import psutil
+from datetime import datetime,timedelta
 import time
 import win32process
+import threading
 
 
 class TimeTracker:
+    # Dictionary that contains all the applications that the user has opened over time
+    __active_applications = {}
+    # Dictionary that contains the active time of all the application that has been open
     __active_time = {}
-    __process_time = {}
+    # Dictionary that contains the active usage time of all the application that has been open
+    __active_usage_time = {}
+    # Dictionary that contains the present time and interact with ^ to give the active usage time of an app
     __time_stamp = {}
 
-    def eachAppUsageTime(self,lastApp=""):
-            current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
-            self.__time_stamp[current_app] = int(time.time())
+    def each_app_usage_time(self):
+        # Get the name of the application that the user is currently using
+        current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
+        # Get the pid of the application that the user is currently using
+        current_app_pid = (win32process.GetWindowThreadProcessId(GetForegroundWindow())[1])
+        # Run another functions at the same time
+        threading.Thread(target=self.__new_program_open,args=(current_app,current_app_pid)).start()
+        threading.Thread(target=self.__stop_watch, args=(current_app, current_app_pid)).start()
+
+        # This will be running all the time in the background, so it doesn't matter that doesn't run now
+        #self.__check_programs_active()
+
+    def __new_program_open(self,currentApp,currentAppPid):
+        if currentApp not in self.__active_applications.values():
+            self.__active_usage_time[currentApp] = 0
+            self.__active_applications[currentAppPid] = currentApp
+            self.__active_time[currentApp] = (time.time())
+            self.__get_open_applications()
+
+    def __check_programs_active(self):
+        for activePrograms in self.__active_applications:
+            if activePrograms not in (program.ppid() for program in psutil.process_iter()):
+                print(str(self.__active_applications[activePrograms]) + " has been closed")
+
+    def __stop_watch(self,currentApp,currentAppPid):
+        while currentAppPid == (win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]):
+            self.__time_stamp[currentApp] = int(time.time())
             time.sleep(1)
-            if current_app not in self.__process_time.keys():
-                self.__process_time[current_app] = 0
-                self.__getOpenApplications()
-            if current_app not in self.__active_time.keys():
-                self.__active_time[current_app] = self.__time_stamp[current_app]
-            self.__process_time[current_app] = self.__process_time[current_app] + int(time.time()) - self.__time_stamp[current_app]
-            print(self.__process_time,self.__process_time.__len__())
-            self.eachAppUsageTime()
+            self.__active_usage_time[currentApp] = self.__active_usage_time[currentApp] + int(time.time()) - \
+                                                    self.__time_stamp[currentApp]
+            print(self.__active_usage_time)
+        self.each_app_usage_time()
 
-    def __getOpenApplications(self):
-        return self.__process_time.__len__()
-
-    #The programmer needs to find the ways to get the time for all the programs running, even when they are not in use.
-    def __checkIfProcessRunning(processName):
-        # Iterate over the all the running process
-        for proc in psutil.process_iter():
-            try:
-                # Check if process name contains the given name string.
-                if processName.lower() in proc.name().lower():
-                    return True
-            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                pass
-        return False
+    def __get_open_applications(self):
+        return self.__active_applications.__len__()
