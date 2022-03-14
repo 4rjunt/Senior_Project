ac90e65f6e089cec364500c13eb550d5fed3e4c1
4rjunt <arielnunez040@gmail.com>
2022-03-14 09:55:27 -0400

    Update 3 of the TimeTrackClass
    Constructor added, now the constructor take care of running all the algorithms, handling store file by the constructor.
    Comments updated.

--- Code/TimeTrackClass.py
+++ Code/TimeTrackClass.py
@@ -16,20 +16,31 @@ import multiprocessing
 
 class TimeTracker:
     # Dictionary that contains all the applications that the user has opened over time
-    # __active_applications[ApplicationPid] = ApplicationPid:ApplicationName
-    __active_applications = {}
+    __active_applications = {}  # ApplicationPid:ApplicationName
     # Dictionary that contains the active time of all the application that has been open
-    # __active_time[ApplicationName] = ApplicationName:ApplicationTotalTimeActive
-    __active_time = {}
+    __active_time = {}  # ApplicationName:ApplicationTotalTimeActive
     # Dictionary that contains the active usage time of all the application that has been open
-    # __active_usage_time[ApplicationName] = ApplicationName:ApplicationUsageTime
-    __active_usage_time = {}
+    __active_usage_time = {}  # ApplicationName:ApplicationUsageTime
     # Dictionary that contains the present time and interact with ^ to give the active usage time of an app
-    # __time_stamp[ApplicationName] = ApplicationName:CurrentTime
-    __time_stamp = {}
+    __time_stamp = {}  # ApplicationName:CurrentTime
 
+    # Variable that store the total amount of time that the user has spent on the computer
+    __computer_active_time = 0
+
+    __today_date = 0
+
+    # Control variable that shows the current status of the checking active programs process
     __active_checking_enable = False
+    # List that contains all the pid of the applications that are being taking care of by a process
     __active_checking_applications = []
+
+    # Constructor
+    def __init__(self,active_applications={},active_time={},active_usage_time={}):
+        if len(active_applications) > 0:
+            #Assignt the arguments passed to the dictionary and start the function to check the active checking apps
+            pass
+        self.__general_stop_watch()
+        self.each_app_usage_time()
 
     # Execute an algorithm that calculate and store the usage time of all the application that has been open or will be open
     def each_app_usage_time(self):
@@ -43,14 +54,12 @@ class TimeTracker:
             multiprocessing.Process(target=self.__check_programs_active,daemon=True).run()
             self.__active_checking_enable = True
 
-    # Creates a new item in the 3 dictionaries, the first dictionary contain the name of the program and its pid
-    # The second one contain the name of the program and the program total time active,
-    # And the third one contain the name of the program and the program usage time.
+    # Creates a new item in the 3 dictionaries
     def __new_program_open(self,currentApp,currentAppPid):
         if currentApp not in self.__active_applications.values():
-            self.__active_usage_time[currentApp] = 0
-            self.__active_applications[currentAppPid] = currentApp
-            self.__active_time[currentApp] = 0
+            self.__active_applications[currentAppPid] = currentApp  # Give the pid of the program and its name
+            self.__active_usage_time[currentApp] = 0  # Give the name of the program and the program usage time.
+            self.__active_time[currentApp] = 0  # Give the name of the program and the program total time active
 
     # Calculate the usage time of the application that the user is currently using
     def __stop_watch(self,currentApp,currentAppPid):
@@ -69,6 +78,14 @@ class TimeTracker:
             self.__active_time[self.__active_applications[active_program]] += int(time.time()) - current_time
             print(self.__active_time)
 
+    # Calculates the active time of the computer(The time that the computer has been used)
+    def __general_stop_watch(self):
+        current_time = int(time.time())
+        time.sleep(1)
+        self.__computer_active_time += int(time.time()) - current_time
+        self.__general_stop_watch()
+
+
     # Executes an algorithm that check when a program is closed, while calculate the active time of all the programs opened
     def __check_programs_active(self):
         while True:
@@ -76,6 +93,11 @@ class TimeTracker:
                 p = multiprocessing.Process(target=self.__stop_watch2(activePrograms),daemon=True)
                 p.start()
                 p.join()
+
+    # Checks if the process is still running, and if its re-run the stopwatch for that specific program,
+    # if its not delete it from the dictionary __active_checking_applications
+    def __check_if_open(self):
+        pass
 
     # Return the number of opened apps
     def get_open_applications(self):
