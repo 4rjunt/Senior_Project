19845f752129803c050a9b7a0c090dfd6bd92974
4rjunt <arielnunez040@gmail.com>
2022-03-18 09:57:33 -0400

    Comments updated and Clock function added
    The Comments were updated, some redundant comments were eliminated, and other were changed, a clock function was added and initialized in the constructor, the function is as the name implies a clock, that gives the actual time and date

---
+++ Code/Test.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+import time
+
+while True:
+    _date1 = datetime.today()
+    print(_date1)
+    time.sleep(.5)
--- Code/TimeTrackClass.py
+++ Code/TimeTrackClass.py
@@ -13,7 +13,6 @@ import win32process
 import threading
 import multiprocessing
 
-
 class TimeTracker:
     # Dictionary that contains all the applications that the user has opened over time
     __active_applications = {}  # ApplicationPid:ApplicationName
@@ -27,18 +26,20 @@ class TimeTracker:
     # Variable that store the total amount of time that the user has spent on the computer
     __computer_active_time = 0
 
-    __today_date = 0
+    today_date = 0
 
     # Control variable that shows the current status of the checking active programs process
     __active_checking_enable = False
     # List that contains all the pid of the applications that are being taking care of by a process
     __active_checking_applications = []
 
-    # Constructor
     def __init__(self,active_applications={},active_time={},active_usage_time={}):
         if len(active_applications) > 0:
             #Assignt the arguments passed to the dictionary and start the function to check the active checking apps
             pass
+        __clock_process = multiprocessing.Process(target=self.__datetime_today(),daemon=True)
+        __clock_process.start()
+        __clock_process.join()
         self.__general_stop_watch()
         self.each_app_usage_time()
 
@@ -47,7 +48,6 @@ class TimeTracker:
         # Get the name and pid of the application that the user is currently using
         current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
         current_app_pid = (win32process.GetWindowThreadProcessId(GetForegroundWindow())[1])
-        # Run another 2 functions at the same time
         threading.Thread(target=self.__new_program_open,args=(current_app,current_app_pid)).start()
         threading.Thread(target=self.__stop_watch, args=(current_app, current_app_pid)).start()
         if not self.__active_checking_enable:
@@ -57,9 +57,9 @@ class TimeTracker:
     # Creates a new item in the 3 dictionaries
     def __new_program_open(self,currentApp,currentAppPid):
         if currentApp not in self.__active_applications.values():
-            self.__active_applications[currentAppPid] = currentApp  # Give the pid of the program and its name
-            self.__active_usage_time[currentApp] = 0  # Give the name of the program and the program usage time.
-            self.__active_time[currentApp] = 0  # Give the name of the program and the program total time active
+            self.__active_applications[currentAppPid] = currentApp
+            self.__active_usage_time[currentApp] = 0
+            self.__active_time[currentApp] = 0
 
     # Calculate the usage time of the application that the user is currently using
     def __stop_watch(self,currentApp,currentAppPid):
@@ -99,14 +99,17 @@ class TimeTracker:
     def __check_if_open(self):
         pass
 
-    # Return the number of opened apps
+    def __datetime_today(self):
+        while True:
+            self.today_date = datetime.today()
+            # Mientras esta funcion corre, otra funcion debe ser la responsable de checar si la fecha cambio
+            # Todos los tiempos de uso incluidos el tiempo actual deben ser guardados para cuando el progranma se cierre
+
     def get_open_applications(self):
         return self.__active_applications.__len__()
 
-    # Return the entire dictionary of __active_usage_time
     def get_active_usage_time(self):
         return self.__active_usage_time
 
-    # Return the entire dictionary of __active_time
     def get_active_time(self):
         return self.__active_time
